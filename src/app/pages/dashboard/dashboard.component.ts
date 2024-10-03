1114fac9e9e07512c36c364ec1d4da9005a67343
shruti.p <shruti.p@trezi.com>
2024-10-03 15:36:48 +0530

    #Added dashboard.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  selectedFlightDate: string = '';  // User-selected flight date
+  selectedRoomType: string = '';    // User-selected hotel room type
+  selectedTrainDate: string = '';   // User-selected train date
+
+  constructor() { }
+
+  bookFlight() {
+    if (this.selectedFlightDate) {
+      alert(`Flight booked on ${this.selectedFlightDate}`);
+    } else {
+      alert('Please select a flight date');
+    }
+  }
+
+  bookHotel() {
+    if (this.selectedRoomType) {
+      alert(`Hotel booked: ${this.selectedRoomType}`);
+    } else {
+      alert('Please select a room type');
+    }
+  }
+
+  bookTrain() {
+    if (this.selectedTrainDate) {
+      alert(`Train booked on ${this.selectedTrainDate}`);
+    } else {
+      alert('Please select a train date');
+    }
+  }
+
+  bookPackage() {
+    alert('Holiday Package booked!');
+  }
 }
